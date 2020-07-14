01ed9b0b02dcc64b82619f5252835d01e31fb6bf
Ali Parvizi <snparvizi75@gmail.com>
2020-07-14 15:34:19 +0430

    thread setup

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <thread>         // std::thread
 
 using namespace std;
 
+void producer() {
+	while(1)
+	cout << "from producer" << endl;
+}
+
+void consumer() {
+	while(1)
+	cout << "from consumer" << endl;
+}
+
 int main() {
+	thread _producer(producer);
+	thread _consumer(consumer);
+
+
 	cout << "Os project started" << endl;
+	
+	_producer.join();
+	_consumer.join();
 
 	return 0;
 }
