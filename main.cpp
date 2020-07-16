a17c9cd5c572c8ce3074f2bce17fc0860067b4ac
Ali Parvizi <snparvizi75@gmail.com>
2020-07-17 02:10:41 +0430

    srt

--- main.cpp
+++ main.cpp
@@ -44,9 +44,24 @@ public:
 	}
 };
 
+class SRT {
+public:
+	int operator()(const Context* c1, const Context* c2) {
+		int c1_a = c1->getRemainingTime();
+		int c2_a = c2->getRemainingTime();
+		if (c1_a == c2_a)
+			c1->getPid() > c2->getPid();
+		return c1_a > c2_a;
+	}
+
+	static int getTimeQ(Context* c) {
+		return 1;
+	}
+};
+
 
 int main() {
-	Producer<SJF> producer("data/producer.ctx");
+	Producer<SRT> producer("data/producer.ctx");
 
 	producer.tick();
 	return 0;
